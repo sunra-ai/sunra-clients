c20dabdb756de220fa25bf36de52242b5b74c733
banyudu <banyudu@gmail.com>
2025-06-12 12:09:22 +0800

    feat: update the demo python code

--- examples/demo-python/speech-to-text.py
+++ examples/demo-python/speech-to-text.py
@@ -1,10 +1,5 @@
 import sunra_client
 
-def on_queue_update(update):
-    if isinstance(update, sunra_client.InProgress):
-        for log in update.logs:
-           print(log["message"])
-
 result = sunra_client.subscribe(
     "elevenlabs/scribe-v1/speech-to-text",
     arguments={
@@ -13,6 +8,7 @@ result = sunra_client.subscribe(
         "tag_audio_events": True,
         "speaker_diarization": False
     },
-    on_queue_update=on_queue_update,
+    with_logs=True,
+    on_queue_update=print
 )
 print(result)
